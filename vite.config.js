2d2d70a692a616e21baa19c21d8aafb9ff6fd1a4
Carol0127 <a52524444@gmail.com>
2026-02-19 00:51:32 +0800

    chore: 初始化專案與字型環境設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  scss: {
+    api: "modern-compiler", // 確保這行有加上
+    silenceDeprecations: [
+      "import", // <--- 針對 @import 警告消音
+      "color-functions",
+      "global-builtin",
+    ],
+    quietDeps: true,
+  },
   base: process.env.NODE_ENV === "production" ? "/LaNuitPuff/" : "/",
   plugins: [react()],
 });
